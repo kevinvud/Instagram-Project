6a1faababa9de56e458c3e30f60a19d52fdb8191
kevinvud <kevinvud@gmail.com>
2017-10-31 11:53:57 -0700

    Implement search

--- Instagram Project/Controller/UserSearchController.swift
+++ Instagram Project/Controller/UserSearchController.swift
@@ -7,20 +7,22 @@
 //
 
 import UIKit
+import Firebase
 
 private let reuseIdentifier = "Cell"
 
-class UserSearchController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+class UserSearchController: UICollectionViewController, UICollectionViewDelegateFlowLayout, UISearchBarDelegate {
+    var users = [User]()
+    var filteredUsers = [User]()
     
-    let searchBar: UISearchBar = {
+    lazy var searchBar: UISearchBar = {
         let sb = UISearchBar()
         sb.placeholder = "Enter username"
         sb.barTintColor = .gray
         UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).backgroundColor = UIColor.rgb(red: 240, green: 240, blue: 240)
+        sb.delegate = self
         return sb
     }()
-    
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,19 +32,45 @@ class UserSearchController: UICollectionViewController, UICollectionViewDelegate
         navigationController?.navigationBar.addSubview(searchBar)
         let navBar = navigationController?.navigationBar
         searchBar.anchor(top: navBar?.topAnchor, left: navBar?.leftAnchor, bottom: navBar?.bottomAnchor, right: navBar?.rightAnchor, paddingTop: 0, paddingLeft: 8, paddingBottom: 0, paddingRight: 8, width: 0, height: 0)
+        
+        fetchUsers()
     }
-
+    
+    private func fetchUsers(){
+        
+        let ref = Database.database().reference().child("users")
+        ref.observe(.value, with: { (snapshot) in
+            guard let dictionaries = snapshot.value as? [String: Any] else {return}
+            dictionaries.forEach({ (key,value) in
+                guard let userDictionary = value as? [String: Any] else {return}
+                let user = User(uid: key, dictionary: userDictionary)
+                self.users.append(user)
+            })
+            
+            self.users.sort(by: { (user1, user2) -> Bool in
+                return user1.userName.compare(user2.userName) == .orderedAscending
+            })
+            self.filteredUsers = self.users
+            self.collectionView?.reloadData()
+        }) { (error) in
+            print("Failed",error)
+        }
+    
+    
+    
+    }
 
 
 }
 extension UserSearchController {
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return filteredUsers.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as? UserSearchCell{
+            cell.user = filteredUsers[indexPath.item]
             return cell
         }
         return UICollectionViewCell()
@@ -52,5 +80,24 @@ extension UserSearchController {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: view.frame.width, height: 66)
     }
+    
+}
+
+extension UserSearchController {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.isEmpty {
+            filteredUsers = users
+        }else{
+            filteredUsers = self.users.filter { (user) -> Bool in
+                return user.userName.lowercased().contains(searchText.lowercased())
+            }
+        }
+        
+        self.collectionView?.reloadData()
+       
+    }
+    
+    
     
 }
--- Instagram Project/View/UserSearchCell.swift
+++ Instagram Project/View/UserSearchCell.swift
@@ -9,6 +9,15 @@
 import UIKit
 
 class UserSearchCell: UICollectionViewCell {
+    
+    var user: User? {
+        didSet{
+            userNameLabel.text = user?.userName
+            guard let profileImageUrl = user?.profileImageUrl else { return}
+            profileImageView.loadImage(urlString: profileImageUrl)
+        }
+    }
+    
     let profileImageView: CustomImageView = {
         let iv = CustomImageView()
         iv.backgroundColor = .red
