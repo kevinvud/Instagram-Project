06f038522cec3093afa6eeb052a184751e04bb16
kevinvud <kevinvud@gmail.com>
2017-11-01 20:11:09 -0700

    Save photo to phone's library, show saved message and animate in and out

--- Instagram Project/Controller/CameraController.swift
+++ Instagram Project/Controller/CameraController.swift
@@ -90,9 +90,13 @@ class CameraController: UIViewController, AVCapturePhotoCaptureDelegate {
     func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photoSampleBuffer: CMSampleBuffer?, previewPhoto previewPhotoSampleBuffer: CMSampleBuffer?, resolvedSettings: AVCaptureResolvedPhotoSettings, bracketSettings: AVCaptureBracketedStillImageSettings?, error: Error?) {
         let imageData = AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer: photoSampleBuffer!, previewPhotoSampleBuffer: previewPhotoSampleBuffer)
         let previewImage = UIImage(data: imageData!)
-        let previewImageView = UIImageView(image: previewImage)
-        view.addSubview(previewImageView)
-        previewImageView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        
+        let containerView = PreviewPhotoContrainerView()
+        containerView.previewImageView.image = previewImage
+        view.addSubview(containerView)
+        containerView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        
+
     }
     
 }
---
+++ Instagram Project/View/PreviewPhotoContainerView.swift
@@ -0,0 +1,96 @@
+//
+//  PreviewPhotoContainerView.swift
+//  Instagram Project
+//
+//  Created by PoGo on 11/1/17.
+//  Copyright © 2017 PoGo. All rights reserved.
+//
+
+import UIKit
+import Photos
+
+class PreviewPhotoContrainerView: UIView {
+    
+    let previewImageView: UIImageView = {
+       let iv = UIImageView()
+        return iv
+    }()
+    
+    let cancelButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "cancel_shadow").withRenderingMode(UIImageRenderingMode.alwaysOriginal), for: .normal)
+        button.addTarget(self, action: #selector(handleCancel), for: .touchUpInside)
+        return button
+    }()
+    
+    
+    let saveButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "save_shadow").withRenderingMode(UIImageRenderingMode.alwaysOriginal), for: .normal)
+        button.addTarget(self, action: #selector(handleSave), for: .touchUpInside)
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super .init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("fatal error in preview photo container view")
+    }
+    
+    func setupViews(){
+        addSubview(previewImageView)
+        addSubview(cancelButton)
+        addSubview(saveButton)
+        
+        saveButton.anchor(top: nil, left: leftAnchor, bottom: bottomAnchor, right: nil, paddingTop: 0, paddingLeft: 24, paddingBottom: 24, paddingRight: 0, width: 50, height: 50)
+        cancelButton.anchor(top: topAnchor, left: leftAnchor, bottom: nil, right: nil, paddingTop: 12, paddingLeft: 12, paddingBottom: 0, paddingRight: 0, width: 50, height: 50)
+        previewImageView.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        
+        
+    }
+    
+    @objc func handleCancel() {
+        self.removeFromSuperview()
+    }
+    
+    @objc func handleSave(){
+        
+        guard let previewImage = previewImageView.image else {return}
+        let library = PHPhotoLibrary.shared()
+        library.performChanges({
+            PHAssetChangeRequest.creationRequestForAsset(from: previewImage)
+        }) { (success, error) in
+            if let error = error {
+                print("Failed to save photo", error)
+            }
+            DispatchQueue.main.async {
+                let savedLabel = UILabel()
+                savedLabel.text = "Saved Succesfully"
+                savedLabel.textColor = .white
+                savedLabel.numberOfLines = 0
+                savedLabel.textAlignment = .center
+                savedLabel.font = UIFont.boldSystemFont(ofSize: 18)
+                savedLabel.backgroundColor = UIColor(white: 0, alpha: 0.3)
+                savedLabel.frame = CGRect(x: 0, y: 0, width: 150, height: 80)
+                savedLabel.center = self.center
+                self.addSubview(savedLabel)
+                savedLabel.layer.transform = CATransform3DMakeScale(0, 0, 0)
+                UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+                    savedLabel.layer.transform = CATransform3DMakeScale(1, 1, 1)
+                }, completion: { (completed) in
+                    UIView.animate(withDuration: 0.5, delay: 0.75, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+                        savedLabel.layer.transform = CATransform3DMakeScale(0.1, 0.1, 0.1)
+                        savedLabel.alpha = 0
+                    }, completion: { (_) in
+                        savedLabel.removeFromSuperview()
+                        
+                    })
+                })
+            }
+        }
+        
+    }
+}
